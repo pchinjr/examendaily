74115d8a2f8192149959787da2c2fc083062eb04
Paul Chin Jr <pchinjr@gmail.com>
2021-09-12 17:22:03 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Be still - become aware of God's presence.
+
+// Be thankful - review the moments of your day with gratitude
+
+// Be present - review your day with curiosity rather than judgement
+
+// Be prayerful - choose one part of your day and pray about it
+
+// Be hopeful - look toward tomorrow
+
+
+// 
